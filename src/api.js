91339929bf7e11c7b4b72868429fa9989ef1403c
JamieYau <jamie.yau2002@me.com>
2023-12-24 20:35:01 +0000

    Refactor fetchNBAGifs function to handle errors

--- src/api.js
+++ src/api.js
@@ -2,17 +2,18 @@ import { GiphyFetch } from "@giphy/js-fetch-api";
 
 const gf = new GiphyFetch(import.meta.env.VITE_GIPHY_API_KEY);
 
-export async function fetchNBAGifs(
-  searchTerm = "NBA",
-  limit = 6,
-  type = "gifs"
-) {
-  const { data } = await gf.search(searchTerm, { limit, type });
-  return data.map((gif) => ({
-    id: gif.id,
-    name: gif.title
-      .split(" GIF")[0]
-      .replace(/(^\w{1})|(\s+\w{1})/g, (letter) => letter.toUpperCase()),
-    imageUrl: gif.images.fixed_height.url,
-  }));
+export async function fetchNBAGifs(searchTerm = "NBA", limit = 6, type = "") {
+  try {
+    const { data } = await gf.search(searchTerm, { limit, type });
+    return data.map((gif) => ({
+      id: gif.id,
+      name: gif.title
+        .split(" GIF")[0]
+        .replace(/(^\w{1})|(\s+\w{1})/g, (letter) => letter.toUpperCase()),
+      imageUrl: gif.images.fixed_height.url,
+    }));
+  } catch (error) {
+    console.error(`Error fetching ${searchTerm} gifs:`, error);
+    return [];
+  }
 }
