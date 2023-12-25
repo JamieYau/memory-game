b6619b17a32722990e8ecde0434da08f2bda9e65
JamieYau <jamie.yau2002@me.com>
2023-12-25 06:54:12 +0000

    Add information modal toggle

--- src/components/StartScreen.jsx
+++ src/components/StartScreen.jsx
@@ -1,17 +1,42 @@
+import { useState } from "react";
 import PropTypes from "prop-types";
+import "../styles/StartScreen.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleInfo,faXmark } from "@fortawesome/free-solid-svg-icons";
 
 export default function StartScreen({ onStart }) {
+  const [showInstructions, setShowInstructions] = useState(false);
+
   return (
     <div className="startScreen">
-      <h1>NBA Memory Game</h1>
-      <p>
-        Click on each card only once. After each click, the cards will be
-        shuffled. Win by clicking all the cards without clicking the same one
-        twice.
-      </p>
-      <button onClick={() => onStart("easy")}>Easy</button>
-      <button onClick={() => onStart("medium")}>Medium</button>
-      <button onClick={() => onStart("hard")}>Hard</button>
+      <div className="title">
+        <h1>NBA</h1>
+        <h1>Memory Game</h1>
+      </div>
+      <div className="difficulty-container">
+        <button onClick={() => onStart("easy")}>Easy</button>
+        <button onClick={() => onStart("medium")}>Medium</button>
+        <button onClick={() => onStart("hard")}>Hard</button>
+      </div>
+      <footer>
+        <button onClick={() => setShowInstructions(true)}>
+          <FontAwesomeIcon icon={faCircleInfo} />
+        </button>
+        {showInstructions && (
+          <div className="modal">
+            <div className="modal-content instructions">
+              <p>
+                Click on each card only once. After each click, the cards will
+                be shuffled. Win by clicking all the cards without clicking the
+                same one twice.
+              </p>
+              <button onClick={() => setShowInstructions(false)}>
+                <FontAwesomeIcon icon={faXmark} />
+              </button>
+            </div>
+          </div>
+        )}
+      </footer>
     </div>
   );
 }
